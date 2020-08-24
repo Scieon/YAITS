d7b035f6601002c073564924e3b0d4a6d3328b12
Anhkhoi Vu-Nguyen <anhkhoi.vunguyen@gmail.com>
2020-08-23 23:09:46 -0400

    add db unit tests and run them on pipeline

--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,11 @@ module github.com/YAITS/api
 go 1.13
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.1
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.4.0
 	go.uber.org/zap v1.15.0
 )
---
+++ api/persistence/db_test.go
@@ -0,0 +1,167 @@
+package persistence
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	_ "github.com/stretchr/testify/assert"
+	_ "github.com/stretchr/testify/require"
+)
+
+const (
+	IssueID = int64(1)
+	Summary = "This is a summary"
+	Description = "This is a description"
+	Assignee = "John Doe"
+	Status = "Open"
+	Priority = int64(1)
+	CreateDate = "some date"
+)
+
+
+func TestMysqlStorage_RetrieveIssueByID(t *testing.T) {
+	// setup
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	// force db closure at end of test
+	defer func() {
+		_ = db.Close()
+	}()
+
+	testingStorage := NewMysqlStorage(db)
+
+	mock.ExpectQuery("SELECT (.+) FROM issues").
+		WithArgs(IssueID).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "summary", "description", "priority", "status", "assignee", "createDate"}).
+			AddRow(IssueID, Summary, Description, Priority, Status, Assignee, CreateDate))
+
+	// run the code
+	if _, err = testingStorage.RetrieveIssueByID(IssueID); err != nil {
+		t.Errorf("Error should not have occurred while updating issue: %s", err)
+	}
+
+	//check expectations are met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Expectations not met: %s", err)
+	}
+}
+
+func TestMysqlStorage_RetrieveIssueByStatus(t *testing.T) {
+	// setup
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	// force db closure at end of test
+	defer func() {
+		_ = db.Close()
+	}()
+
+	testingStorage := NewMysqlStorage(db)
+
+	mock.ExpectQuery("SELECT (.+) FROM issues WHERE status").
+		WithArgs(Status).
+		WillReturnRows(sqlmock.NewRows([]string{"id", "summary", "description", "priority", "status", "assignee", "createDate"}).
+			AddRow(IssueID, Summary, Description, Priority, Status, Assignee, CreateDate))
+
+	// run the code
+	if _, err = testingStorage.RetrieveIssueByStatus(Status); err != nil {
+		t.Errorf("Error should not have occurred while updating issue: %s", err)
+	}
+
+	//check expectations are met
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("Expectations not met: %s", err)
+	}
+}
+
+func TestMysqlStorage_UpdateIssue(t *testing.T) {
+	// setup
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	// force db closure at end of test
+	defer func() {
+		_ = db.Close()
+	}()
+
+	testingStorage := NewMysqlStorage(db)
+
+	t.Run("NoError", func(t *testing.T) {
+		mock.ExpectQuery("SELECT (.+) FROM issues").
+			WithArgs(IssueID).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "summary", "description", "priority", "status", "assignee", "createDate"}).
+				AddRow(IssueID, Summary, Description, Priority, Status, Assignee, CreateDate))
+
+		// set expectations
+		mock.ExpectExec("UPDATE issues SET").
+			WithArgs(Summary, Description, Assignee, Status, Priority, IssueID).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// run the code
+		if _, err = testingStorage.UpdateIssue(Summary, Description, Assignee, Status, Priority, IssueID); err != nil {
+			t.Errorf("Error should not have occurred while updating issue: %s", err)
+		}
+
+		//check expectations are met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Expectations not met: %s", err)
+		}
+	})
+}
+
+func TestMysqlStorage_DeleteIssueByID(t *testing.T) {
+	// setup
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	// force db closure at end of test
+	defer func() {
+		_ = db.Close()
+	}()
+
+	testingStorage := NewMysqlStorage(db)
+
+	t.Run("NoError", func(t *testing.T) {
+		// set expectations
+		mock.ExpectExec("DELETE FROM issues").
+			WithArgs(IssueID).
+			WillReturnResult(sqlmock.NewResult(1, 1))
+
+		// run the code
+		if err = testingStorage.DeleteIssueByID(IssueID); err != nil {
+			t.Errorf("Error should not have occurred while deleting issue: %s", err)
+		}
+
+		//check expectations are met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Expectations not met: %s", err)
+		}
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		// set expectations
+		mock.ExpectExec("DELETE FROM issues").
+			WithArgs(IssueID).
+			WillReturnError(errors.New("err"))
+
+		// run the code
+		if err = testingStorage.DeleteIssueByID(IssueID); err == nil {
+			t.Errorf("Error should have occured while deleting issue: %s", err)
+		}
+
+		//check expectations are met
+		if err := mock.ExpectationsWereMet(); err != nil {
+			t.Errorf("Expectations not met: %s", err)
+		}
+	})
+}
